9ea8cfb77d0378ae5eb0966738e60f95e0a708a1
Yaojema <yaojema@gmail.com>
2024-03-15 21:48:50 -0500

    ⚙FIX: Se soliciona el bug de encriptar - Se logra quitar el bug cambiando la posicion de la vocal {a} - Issue: #14

--- main.js
+++ main.js
@@ -1,64 +1,45 @@
-
-// contador de vocales
-// function vowels(msg){
-//     // let cadena = "vocaleeaas";
-//     let numeroVocales = msg.match(/[aeiou]/gi).length;
-//     // console.log(numeroVocales);
-//     return numeroVocales
-// }
 // true = encriptar
 // false = desencriptar
-function encriptar(msg, mode = true){
+function encriptar(msg, isEnc = true){
     let matriz = [
-        ['a','ai'],
         ['e','enter'],
         ['i','imes'],
+        ['a','ai'],
         ['o','ober'],
         ['u','ufat'],
     ];
-    if(mode){
+    if(isEnc){
         // recorre la matriz 5 veces - 0-4
         for (let i = 0; i < matriz.length; i++) { 
             // Verifica si el mensaje tiene la vocal actual de la interacción [a-e-i-o-u]
-            // Verifica si el mensaje tiene la vocal actual de la interacción [0-1-2-3-4]
-            // inicia con matriz[0][0] = a
-            // inicia con matriz[1][0] = e
-            // inicia con matriz[2][0] = i
-            // inicia con matriz[3][0] = o
-            // inicia con matriz[4][0] = u
-            if(msg.includes(matriz[i][0])){
-                console.log("ACA: " + matriz[i][0]);
+                if(msg.includes(matriz[i][0])){
+                // console.log("ACA: " + matriz[i][0]);
                 // Reemplaza la coincidencia
                 msg = msg.replaceAll(matriz[i][0], matriz[i][1]);
-                console.log("ACA2: " + matriz[i][1]);
+                // console.log("ACA2: " + matriz[i][1]);
             }
-            console.log('Encriptado');
+            // console.log('Encriptado');
         }
     } else {
-        let newMatrix = matriz;
-        console.log(newMatrix);
-        for (let i = 0; i < newMatrix.length; i++) {
+        for (let i = 0; i < matriz.length; i++) {
             // Verifica si el mensaje tiene el patron actual de la interacion
-            if(msg.includes(newMatrix[0][i])){
+            if(msg.includes(matriz[i][1])){
                 // Reemplaza la coincidencia
-                msg = msg.replaceAll(newMatrix[i][0], newMatrix[i][1]);
-                
+                msg = msg.replaceAll(matriz[i][1], matriz[i][0]);
             }
-            console.log('Desencriptado: Times');
+            // console.log('Desencriptado: Times');
         }
     }
-    // console.log(msg);
     return msg
 }
 console.log(
-    encriptar('hola')
-    // +" : "+"otros"+" - "+
-    // encriptar('a')+" - "+
-    // encriptar('e')+" - "+
-    // encriptar('i')+" - "+
-    // encriptar('o')+" - "+
-    // encriptar('u')
+    'Encriptado: '+
+    encriptar('murcielago')
     // encriptar('murcielago')
     // mufatrcimesenterlaigober
     // mufatrcimesenterlaimesgober
+);
+console.log(
+    'Desencriptado: '+
+    encriptar('mufatrcimesenterlaigober', false)+""
 );
